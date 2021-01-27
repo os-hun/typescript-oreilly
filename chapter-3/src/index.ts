10b0755ad8e92c1dc727e221ecbf778590a1bcc7
Shun <shun12baseball@gmail.com>
2021-01-28 08:37:12 +0900

    feat: chapter-3 => any type, unknown type done

--- chapter-3/src/index.ts
+++ chapter-3/src/index.ts
@@ -2,5 +2,22 @@ function squareOf(n: number) {
   return n * n
 }
 
-console.log(squareOf(2))
+console.log('squareOF =>', squareOf(2))
+
+// any
+let any_a: any = 666
+let any_b: any = ['danger']
+let any_c = any_a + any_b
+console.log('any =>', any_c)
+
+// unknown
+let unknown_a: unknown = 30
+let unknown_b = unknown_a === 123
+// let unknown_c = a + 10
+// ↑はエラーになる
+if (typeof unknown_a === 'number') {
+  let unknown_d = unknown_a + 10
+  console.log('unknown_d =>', unknown_d)
+}
+console.log('unknown_b =>', unknown_b)
 
